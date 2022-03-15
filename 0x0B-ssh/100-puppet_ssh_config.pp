fade83b6a66286c2cfafe0d76f641a74f8a3cac4
Zelipha <zeliphawambui756@gmail.com>
2022-03-15 09:03:35 -0700

    Connect server without typing password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
 # SSH configuration file to connect to server without password
 
 exec { "echo":
-  path => "/usr/bin/",
-  command => 'echo "IdentityFile ~/.ssh/school  PassworwordAuthentication  no" >>  /etc/ssh/ssh_config'
+  path => "usr/bin:/bin",
+  command => 'echo "IdentityFile ~/.ssh/school  PassworwordAuthentication  no" >>  /etc/ssh/ssh_config',
+  returns => [0,1],
 }
