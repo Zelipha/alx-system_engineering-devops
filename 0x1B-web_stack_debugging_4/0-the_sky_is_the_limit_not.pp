795af7ac337288570f9c71b379671ae97c009c83
Zelipha <zeliphawambui756@gmail.com>
2022-05-05 18:57:06 +0300

    Increases the amount of traffic an Nginx server can handle

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Fixing the problem of high amount of requests
+# Increases the amount of traffic an Nginx server can handle.
 
 # Increase the ULIMIT of the default file
-exec {'replace':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
-}
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-exec {'nginx-restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
